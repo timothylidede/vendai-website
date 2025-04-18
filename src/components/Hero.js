f67a1f7839930b56db8775e85172bb1f6657b4c3
Timothy Lidede <75303411+timothylidede@users.noreply.github.com>
2025-04-18 04:52:28 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
                     <NavBar />
                 </div>
                 
-                <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
+                <div className="m-auto overflow-hidden mx-4 mt-16 lg:mt-12 p-2 md:p-12 h-5/6" data-aos="zoom-in">
 
                     <div id='hero' className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
                         <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">

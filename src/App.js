70c8f73867fdcd2e34d66f0995bc3411447d424e
Lidede <lidedetimothy@gmail.com>
2025-05-15 21:02:40 +0300

    Vendai

--- src/App.js
+++ src/App.js
@@ -1,70 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import './index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+// All pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import DemoProduct from './pages/DemoProduct';
+
+import {useDocTitle} from './components/CustomHook';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  useEffect(() => {
+    const aos_init = () => {
+      AOS.init({
+        once: true,
+        duration: 1000,
+        easing: 'ease-out-cubic',
+      });
+    }
+
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  }, []);
+
+  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <iframe
-        src="https://driving-contributors-755720.framer.app/"
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        title="Vendai"
-      />
-    </div>
+    <>
+      <Router>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/get-demo" element={<DemoProduct />} /> 
+          </Routes>
+        </ScrollToTop>
+      </Router>
+    </>
   );
 }
 
+
 export default App;
-
-
-
-
-// import React, { useEffect } from 'react';
-// import AOS from 'aos';
-// import "aos/dist/aos.css";
-// import './index.css';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from 'react-router-dom';
-// // All pages
-// import Home from './pages/Home';
-// import Contact from './pages/Contact';
-// import DemoProduct from './pages/DemoProduct';
-
-// import {useDocTitle} from './components/CustomHook';
-// import ScrollToTop from './components/ScrollToTop';
-
-// function App() {
-//   useEffect(() => {
-//     const aos_init = () => {
-//       AOS.init({
-//         once: true,
-//         duration: 1000,
-//         easing: 'ease-out-cubic',
-//       });
-//     }
-
-//     window.addEventListener('load', () => {
-//       aos_init();
-//     });
-//   }, []);
-
-//   useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
-
-//   return (
-//     <>
-//       <Router>
-//         <ScrollToTop>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/get-demo" element={<DemoProduct />} /> 
-//           </Routes>
-//         </ScrollToTop>
-//       </Router>
-//     </>
-//   );
-// }
-
-
-// export default App;
